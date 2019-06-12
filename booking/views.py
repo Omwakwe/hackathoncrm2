790d5298ba117d55edd7ee0d8c1126e2964751c1
Starford Omwakwe <starfordomwakwe@gmail.com>
2019-06-12 10:58:14 +0300

    add base.html and styles

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class BookingView(View):
+    # form_class = MyForm
+    template_name = 'booking/booking.html'
+
+    def get(self, request, *args, **kwargs):
+        # form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {})
